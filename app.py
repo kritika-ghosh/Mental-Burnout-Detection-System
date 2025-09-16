97969ad1734f1f4662e29d35b3e8eda808d12984
kritika-ghosh <kri.kritika.ghosh@gmail.com>
2025-09-16 16:39:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1144,4 +1144,4 @@ with gr.Blocks(theme=burnout_theme, css=custom_css) as demo:
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
-    demo.launch(server_name="0.0.0.0", server_port=port)
+    demo.launch(server_name="0.0.0.0", server_port=port, share=True)
